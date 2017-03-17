033670e120c8978740c159659d69ab16b732a6a4
Fernando Sanchez <fernandosanchezmunoz@gmail.com>
2017-03-17 14:04:10 -0400

    Update mount_s3_centos.sh

--- mount_s3_centos.sh
+++ mount_s3_centos.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#Description: mount s3 bucket into cloud instances
-
+#mount s3 bucket into cloud instances
 BUCKET_NAME=fernando-sanchez
 MOUNT_POINT=/mnt/s3
 ACCESS_KEY_ID=AKIAJZGXV3RQ43BD3BMA
@@ -8,19 +7,14 @@ AWS_SECRET_ACCESS_KEY=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
 
 #install requirements
 yum remove fuse fuse-s3fs
-yum install -y gcc gcc-c++ automake openssl-devel libstdc++-devel curl-devel libxml2-devel mailcap wget git \
-ftp://ftp.pbone.net/mirror/ftp.centos.org/7.3.1611/os/x86_64/Packages/fuse-libs-2.9.2-7.el7.x86_64.rpm
-
-#create working dir and CD
-mkdir -p ~/s3
-cd ~/s3
-
+yum install -y gcc gcc-c++ automake openssl-devel libstdc++-devel curl-devel libxml2-devel mailcap wget git screen \
+c
 #install fuse backend for S3 instances
 yum remove fuse fuse-s3fs
 yum install gcc libstdc++-devel gcc-c++ curl-devel libxml2-devel openssl-devel mailcap
 cd /usr/src/
-wget http://downloads.sourceforge.net/project/fuse/fuse-2.X/2.9.3/fuse-2.9.3.tar.gz
 wget https://github.com/libfuse/libfuse/releases/download/fuse_2_9_4/fuse-2.9.3.tar.gz
+tar xvfz fuse-2.9.3.tar.gz
 cd fuse-2.9.3
 ./configure --prefix=/usr/local
 make && make install
@@ -31,12 +25,15 @@ modprobe fuse
 lsmod|grep fuse #check it works
 
 #s3fs
+cd /usr/src
 git clone https://github.com/s3fs-fuse/s3fs-fuse.git
 cd s3fs-fuse
 ./autogen.sh
 ./configure
 make
 sudo make install
+ln -s /usr/local/bin/s3fs /usr/bin/s3fs
+
 
 #create config file
 #configure s3fs required Access Key and Secret Key of your S3 Amazon account (Change AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY with your actual key values).
@@ -47,3 +44,6 @@ touch /etc/passwd-s3fs && chmod 640 /etc/passwd-s3fs && echo "$ACCESS_KEY_ID:$AW
 #create mount point and mount
 mkdir -p $MOUNT_POINT
 s3fs $BUCKET_NAME $MOUNT_POINT -o passwd_file=/etc/passwd-s3fs
+
+#check it works
+grep s3fs /etc/mtab
